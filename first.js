8c37cb2f87f7568338277891f4e0c2cb4907f396
shrutiantil <antilshruti879@gmail.com>
2026-08-18 20:42:13 +0530

    changes

---
+++ first.js
@@ -0,0 +1,13 @@
+const OpenButton = document.querySelector("#open-button");
+const CloseButton = document.querySelector("#close-button");
+
+
+
+OpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+})
+ 
+
+CloseButton.addEventListener("click", () => OpenButton.click() 
+
+)
