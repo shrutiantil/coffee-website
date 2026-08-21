cf91ae08fa6def6268578577a5586fb007eccdf5
shrutiantil <antilshruti879@gmail.com>
2026-08-21 21:13:03 +0530

    changes

--- first.js
+++ first.js
@@ -17,4 +17,22 @@ navLinks.addEventListener("click", (e) => {
     menuBtnIcon.setAttributeNS("class", "ri-menu-3-lines");
 });
 
-const scrollReval
+const scrollRevealOption = {
+    distance:"50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_container h1", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_container p", {
+    ...scrollRevealOption,
+    delay: 500
+});
+
+ScrollReveal().reveal(".header_btns", {
+    ...scrollRevealOption,
+    delay: 1000
+});
